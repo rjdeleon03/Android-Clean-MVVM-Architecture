1b6cb12a0787cd5e4ca0fa534a24d8b0c345bb04
rjdeleon <ricardopablo.deleon@gmail.com>
2020-10-05 22:55:11 +0800

    Initial commit

---
+++ app/src/main/java/com/rjdeleon/memorynotes/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rjdeleon.memorynotes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rjdeleon.memorynotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
